ef3919dfa67690e53a8e92dafe597536d8b10b52
Shinma Takahashi <shinma.takahashi@postas.co.jp>
2026-07-09 14:43:03 +0900

    fragにviewをバインディング

--- app/src/main/java/com/example/weatherapp/ui/WeatherFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/WeatherFragment.kt
@@ -6,15 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.weatherapp.R
+import com.example.weatherapp.databinding.FragmentWeatherBinding
 import com.example.weatherapp.viewmodel.WeatherViewModel
 
 class WeatherFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = WeatherFragment()
-    }
-
+    private var _binding: FragmentWeatherBinding? = null
+    private val binding get() = _binding!!
     private val viewModel: WeatherViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,9 +21,11 @@ class WeatherFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_weather, container, false)
+        _binding = FragmentWeatherBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
