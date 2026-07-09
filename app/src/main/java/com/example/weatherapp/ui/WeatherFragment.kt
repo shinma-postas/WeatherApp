6db95acb1bf9e342a627323a4f2fe849153ad461
Shinma Takahashi <shinma.takahashi@postas.co.jp>
2026-07-09 15:19:51 +0900

    fragmentのviewを画面に表示

--- app/src/main/java/com/example/weatherapp/ui/WeatherFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/WeatherFragment.kt
@@ -1,18 +1,17 @@
 package com.example.weatherapp.ui
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.weatherapp.databinding.FragmentWeatherBinding
-import com.example.weatherapp.viewmodel.WeatherViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class WeatherFragment : Fragment() {
     private var _binding: FragmentWeatherBinding? = null
     private val binding get() = _binding!!
-    private val viewModel: WeatherViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
