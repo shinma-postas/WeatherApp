d1fb6379183e1a35b59035fc97ad27723bd76131
Shinma Takahashi <shinma.takahashi@postas.co.jp>
2026-07-09 12:47:37 +0900

    fragとvmファイルを作成

---
+++ app/src/main/java/com/example/weatherapp/ui/WeatherFragment.kt
@@ -0,0 +1,32 @@
+package com.example.weatherapp.ui
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.weatherapp.R
+import com.example.weatherapp.viewmodel.WeatherViewModel
+
+class WeatherFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = WeatherFragment()
+    }
+
+    private val viewModel: WeatherViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_weather, container, false)
+    }
+}
